7207872318bcf95c18b29cbda32f070855ef454f
Lino Oropeza <linooropeza@gmail.com>
2020-05-30 21:16:28 -0400

    Updated the matrix class to work with binary numbers

--- electronics.rb
+++ electronics.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Gems
 require 'rpi_gpio'
 require 'json'
@@ -7,28 +9,20 @@ require_relative 'matrix.rb'
 
 RPi::GPIO.set_numbering :bcm
 
-
-
-
 # It clears the board and realeases the GPIO pins
 def clear_board
-	RPi::GPIO.reset
-	puts 'The board has been cleared'
+  RPi::GPIO.reset
+  puts 'The board has been cleared'
 end
 
 # Setup
-rows    = [17, 27, 22, 05, 06, 13, 19, 26]
-columns = [18, 23, 24, 25, 12, 16, 20, 21] 
+rows    = [17, 27, 22, 0o5, 0o6, 13, 19, 26]
+columns = [18, 23, 24, 25, 12, 16, 20, 21]
 matrix = Matrix.new rows, columns
 
 begin
-	while true
-		matrix.string "Bitch please", 0.5
-		sleep 1
-		matrix.string "Yes I am sassy", 0.5
-
-	end 
-	rescue SignalException => e
-    clear_board
+  loop do
+  end
+rescue SignalException => e
+  clear_board
 end
-
--- matrix.rb
+++ matrix.rb
@@ -1,78 +1,84 @@
+# frozen_string_literal: true
+
 require 'rpi_gpio'
 
-class Matrix
+class LED_Matrix
+  DELAY = 0.0000000001
 
-	DELAY = 0.0000000001
-	
-  def initialize rows, columns
-		# Rows and columns are arrays containing the pin values
-		# of the rows and columns of the LED Matrix
-		# This class assumes that the rows are connected to the anodes
-		# of the LEDs and the columns to the cathodes
-		@rows = rows
-		@columns = columns
-		@letters_capital = read_json 'json/capital_letters.json'
-		
-		# Setting up the Matrix to off
-		@rows.each do |pin|
-			RPi::GPIO.setup pin, as: :output, initialize: :low
-		end
-		@columns.each do |pin|
-			RPi::GPIO.setup pin, as: :output, initialize: :high
-		end
-	end 
-	
-	def test
-	count = 0
-		@rows.each do |row|
-			RPi::GPIO.set_high row
-			@columns.each do |column|
-				RPi::GPIO.set_low  column
-				count += 1
-				puts "Count is #{count}"
-				sleep DELAY
-				RPi::GPIO.set_high column
-			end
-			RPi::GPIO.set_low row
-		end
-	end
-	
-	def string string, delay
-		words = string.split(' ')
-		words.each do |word|
-			letters = word.chars
-			letters.each do |letter|
-				frame @letters_capital[letter.upcase], delay
-			end
-			sleep delay
-		end
-	end
-	
-	def frame frame, delay
-		# The frame is a matrix with the same ammount of rows and columns
-		# than the marix
-		time_start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-		loop do
-			frame.each_index do |row|
-				frame[row].each_index do |column|
-					if frame[row][column] == 1 
-						RPi::GPIO.set_high @rows[row]
-						RPi::GPIO.set_low @columns[column]
-						sleep DELAY
-						RPi::GPIO.set_low @rows[row]
-						RPi::GPIO.set_high @columns[column]
-					end
-				end
-			end
-			time_current = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-		break if time_current - time_start > delay
-		end
-	end
+  def initialize(rows, columns)
+    # Rows and columns are arrays containing the pin values
+    # of the rows and columns of the LED Matrix
+    # This class assumes that the rows are connected to the anodes
+    # of the LEDs and the columns to the cathodes
+    @rows = rows
+    @columns = columns
+    @letters_capital = read_json 'capital_letters.json'
 
-	private
-	def read_json json_file
-		json = File.read(json_file)
-		JSON.parse(json)
-	end
+    # Setting up the Matrix to off
+    @rows.each do |pin|
+      RPi::GPIO.setup pin, as: :output, initialize: :low
+    end
+    @columns.each do |pin|
+      RPi::GPIO.setup pin, as: :output, initialize: :high
+    end
+   end
 
+  # TODO: Modify Test Method to work with binary numbers
+  def test
+    count = 0
+    @rows.each do |row|
+      RPi::GPIO.set_high row
+      @columns.each do |column|
+        RPi::GPIO.set_low column
+        count += 1
+        puts "Count is #{count}"
+        sleep DELAY
+        RPi::GPIO.set_high column
+      end
+      RPi::GPIO.set_low row
+    end
+ end
+
+  def string(string, delay)
+    words = string.split(' ')
+    words.each do |word|
+      letters = word.chars
+      letters.each do |letter|
+        frame @letters_capital[letter.upcase], delay
+      end
+      sleep delay
+    end
+  end
+
+  def frame(frame, delay)
+    # The frame is a matrix with the same ammount of rows and columns
+    # than the marix
+    time_start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    loop do
+      @rows.times do |row|
+        @columns.times do |column|
+          next unless ((frame[row] << column) & 0b10000000).positive?
+
+          RPi::GPIO.set_high @rows[row]
+          RPi::GPIO.set_low @columns[column]
+          sleep DELAY
+          RPi::GPIO.set_low @rows[row]
+          RPi::GPIO.set_high @columns[column]
+        end
+      end
+
+      time_current = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      break if time_current - time_start > delay
+    end
+  end
+
+  private
+
+  def read_json(json_file)
+    directory = __dir__
+    directory = File.join directory, json
+    json = File.read File.join(directory, json_file)
+    characters = JSON.parse(json)
+    characters.map { |k, v| [k, v.to_i(2)] }.to_h
+  end
 end
